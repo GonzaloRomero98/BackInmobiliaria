854e7bbb4bda2ee66369e685bb600199372ceabc
Gonzalo <gnrq2024@gmail.com>
2026-09-17 12:40:32 -0300

    ubicacion service

--- src/modules/ubicacion/ubicacion.module.ts
+++ src/modules/ubicacion/ubicacion.module.ts
@@ -4,9 +4,10 @@ import { Region } from "./entities/region.entity";
 import { Ciudad } from "./entities/ciudad.entity";
 import { UbicacionController } from "./ubicacion.controller";
 import { UbicacionService } from "./ubicacion.service";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Ciudad, Region])],
+    imports:[TypeOrmModule.forFeature([Ciudad, Region]),AuthModule],
     controllers:[UbicacionController],
     providers:[UbicacionService],
     exports:[UbicacionService]
