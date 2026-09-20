8b75d48d7cff34b436848a4eb7d8f05ec7daee10
Gonzalo <gnrq2024@gmail.com>
2026-09-19 23:41:29 -0300

    mejores usuario y rol

--- src/modules/usuario/entities/usuario.entity.ts
+++ src/modules/usuario/entities/usuario.entity.ts
@@ -13,7 +13,7 @@ export class Usuario {
     @Column()
     contrasena: string;
 
-    @Column()
+    @Column({unique:true,length:50})
     nombre: string;
 
     @ManyToOne(()=> Rol,{eager:true, nullable:false})
