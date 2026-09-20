835836bdb1f6074c3c10c673c453746160dba576
Gonzalo <gnrq2024@gmail.com>
2026-09-20 17:27:16 -0300

    creacion de la entidad proyecto

--- src/modules/proyecto/proyecto.module.ts
+++ src/modules/proyecto/proyecto.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Proyecto } from "./entities/proyecto.entity";
 import { PoryectoController } from "./proyecto.controller";
 import { ProyectoService } from "./proyecto.service";
+import { Ciudad } from "../ubicacion/entities/ciudad.entity";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Proyecto])],
+    imports:[TypeOrmModule.forFeature([Proyecto, Ciudad]), AuthModule],
     controllers:[PoryectoController],
     providers:[ProyectoService],
     exports:[TypeOrmModule, ProyectoModule]
